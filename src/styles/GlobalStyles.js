9b84fdced2f94d8f990821dc5625bbfa79b53d2f
Eun Byul Cheon <75746836+eunbyulcheon@users.noreply.github.com>
2022-08-01 09:36:31 +0900

    Initial commit

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+    
+    body {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+            'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+            sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    button {
+        border: none;
+        background: none;
+        padding: 0;
+        cursor: pointer;
+    }
+
+    input, textarea { 
+      -moz-user-select: auto;
+      -webkit-user-select: auto;
+      -ms-user-select: auto;
+      user-select: auto;
+    }
+    
+    input:focus {
+      outline: none;
+    }
+`;
+
+export default GlobalStyles;
